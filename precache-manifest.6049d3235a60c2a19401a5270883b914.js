16ea4ae6ffaf2b8033b21128624329f16fd6d80d
Anthony Noel <antjnoel@gmail.com>
2020-09-22 01:57:32 -0500

    Updates

---
+++ precache-manifest.6049d3235a60c2a19401a5270883b914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f075ada68c8dc4ce086181370adc92",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a6f62208168ed7fa4ed9",
+    "url": "/monsters/static/css/main.a20f56ec.chunk.css"
+  },
+  {
+    "revision": "69b6d0a5e8111e032d70",
+    "url": "/monsters/static/js/2.a55489bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.a55489bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f62208168ed7fa4ed9",
+    "url": "/monsters/static/js/main.86e24480.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
